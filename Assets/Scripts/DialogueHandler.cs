37f50caaa4d66f8bb7584ec62e2a3e553c5007ea
braedon911 <53923231+braedon911@users.noreply.github.com>
2022-10-11 13:25:20 -0400

    created choice buttons

--- Assets/Scripts/DialogueHandler.cs
+++ Assets/Scripts/DialogueHandler.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueHandler : MonoBehaviour
 {
     public List<Dialogue> dialogueset;
-    public GameObject choiceWheel;
+    Dialogue currentDialogue;
+    Dictionary<string, bool> currentTags;
 
-    public void NextDialogue()
+    [Header("Text Objects")]
+    public GameObject buttonPrefab;
+    public GameObject buttonPanel;
+    public float width = 647f;
+    List<GameObject> choiceButtons;
+    public TMP_Text textBox;
+
+    public void Start()
+    {
+        currentDialogue = dialogueset[0];
+        LoadCurrentDialogue();
+    }
+    public void Choose(int choice)
     {
 
     }
     public void LoadCurrentDialogue()
     {
+        currentTags = currentDialogue.GetTags();
+        
+        textBox.text = currentDialogue.text;
 
+        foreach (GameObject button in choiceButtons)
+        {
+            Destroy(button);
+        }
+        choiceButtons.Clear();
+
+        if (currentTags.GetValueOrDefault("choices", false))
+        {
+            var count = currentDialogue.ChoiceCount();
+            for (int i = 0; i < count; i++)
+            {
+                var button = Instantiate(buttonPrefab, buttonPanel.transform);
+                choiceButtons.Add(button);
+
+                var buttonTransform = button.GetComponent<RectTransform>();
+                buttonTransform.anchoredPosition = new Vector2(647 / (count + 1), buttonTransform.anchoredPosition.y);
+
+            }
+        }
     }
 }
